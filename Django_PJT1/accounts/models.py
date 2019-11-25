a219a987f9acdcf30fb2ee9944d2ea35c184c50e
Hyun Hee Son <hyunhee18@gmail.com>
2019-11-25 19:55:30 +0900

    191125

--- Django_PJT1/accounts/models.py
+++ Django_PJT1/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+# User < AbstractUser < AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from movies.models import Genre
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
+
+class User(AbstractUser):
+    nickname = models.Charfield(max_length=30)
+    like_genres = models.ManyToManyField(Genre, related_name='like_users')
+
+class Chatroom(models.Model):
+    name = models.Charfield(max_length=200)
+    chat_users = models.ManyToManyField(User, related_name='use_chatroom')
+    styles = models.ManyToManyField(Genre, related_name='chats')
+
+class Image(models.Model):
+    # Image.objects.get(id=1).user
+    # User.objects.get(id=1).image_set.file.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    file = ProcessedImageField(
+        processors=[ResizeToFit(600, 600, mat_color=(256, 256, 256))],
+        upload_to = 'users/images/',
+        format='JPEG',
+        options={'quality': 90},
+    )
+
+class Chatcontents(TimeStampedModel):
+    content = models.Charfield(max_length=300)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
--- Django_PJT1/test_pjt/settings.py
+++ Django_PJT1/test_pjt/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'test_pjt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -115,6 +117,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'accounts.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
